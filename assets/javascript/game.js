e4697979d11aaf6b11b7d078751aed1837a4254a
sun1devil <codejmc11@gmail.com>
2018-04-30 23:36:48 -0700

    fix syntax errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,69 @@
 
-
+ //This is broken    //
 // Declare Variables //
-var numberArray = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120];
-var crystalArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+var numberArray = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
+var crystalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var winCount = 0;
-var lossCount= 0;
-var currentValue = randomNumber + crystalValue;
-var randomNumber = numberArray[Math.floor(random() * numberArray.length)];  
-var crystal1 = crystalArray[Math.floor(random() * crystalArray.length)];  
-var crystal2 = crystalArray[Math.floor(random() * crystalArray.length)];  
-var crystal3 = crystalArray[Math.floor(random() * crystalArray.length)];  
-var crystal4 = crystalArray[Math.floor(random() * crystalArray.length)];   
+var lossCount = 0;
+var currentValue = 0;
+var randomNumber = numberArray[Math.floor(random() * numberArray.length)];
+var crystal1 = crystalArray[Math.floor(random() * crystalArray.length)];
+var crystal2 = crystalArray[Math.floor(random() * crystalArray.length)];
+var crystal3 = crystalArray[Math.floor(random() * crystalArray.length)];
+var crystal4 = crystalArray[Math.floor(random() * crystalArray.length)];
 // functions //
+$('#Crystal1').on('click', function () {
+    currentValue = randomNumber + crystal1;
+    console.log("New Value=" + currentValue);
+    $('#Current').text(currentValue);
+    })
 
-// for loops //
-for (var i=0; i< 101; i++){
-    console.log(randomNumber);
-    numberArray.push(randomNumber);
-}
-for (vari =0; i< 12; i++){
-    console.log(crystal1, crystal2, crystal3, crystal4);
-    crystalArray.push(crystal1, crystal2, crystal3, crystal4);
-}
-// If Statements //
+    $('#Crystal2').on('click', function () {
+        currentValue = randomNumber + crystal2;
+        console.log("New Value=" + currentValue);
+        $('#Current').text(currentValue);
+        })           
+        $('#Crystal3').on('click', function () {
+            currentValue = randomNumber + crystal3;
+            console.log("New Value=" + currentValue);
+            $('#Current').text(currentValue);
+            })
+            $('#Crystal4').on('click', function () {
+            currentValue = randomNumber + crystal4;
+            console.log("New Value=" + currentValue);
+            $('#Current').text(currentValue);
+        })
+                // Create function for reset game //
+                // add timeout to randomNumber // 
+                // link to portfolio page //
 
-    if (currentValue == randomNumber){
-    $('#message').text('Winner! Perfect Match! You are a Genius!');
-    winCount ++;
-    $('#Wins').text(winCount);
-    console.log(wins)
-    // $('#crystals').empty();
-    // newCrystals();
-    // newGame();
-    } 
-    else if ( currentValue > numberToGuess){
-        $('#message').text('Over Valuation! You Lose!');
-      lossCount ++;
-      $('#losses').text(lossCount);
-      console.log(lossCount)
-    }
-    else{
-        $('#message').text('Good Guess, but not a match! Chose another Crystal');
-    }
-    
+                // for loops not used //
+                // for (var i=0; i< 101; i++){
+                //     console.log(randomNumber);
+                //     numberArray.push(randomNumber);
+                // }
+                // for (vari =0; i< 12; i++){
+                //     console.log(crystal1, crystal2, crystal3, crystal4);
+                //     crystalArray.push(crystal1, crystal2, crystal3, crystal4);
+
+                // If Statements //
+
+                if (currentValue == randomNumber) {
+                    $('#message').text('Winner! Perfect Match! You are a Genius!');
+                    winCount++;
+                    $('#Wins').text(winCount);
+                    console.log(wins)
+                    // Resetgame //
+                } else if (currentValue > numberToGuess) {
+                    $('#message').text('Over Valuation! You Lose!');
+                    lossCount++;
+                    $('#losses').text(lossCount);
+                    console.log(lossCount)
+                    // Resetgame //
+                } else {
+                    $('#message').text('Good Guess, but not a match! Chose another Crystal');
+                }
+
     //   $('#crystals').empty();
     //   newCrystals();
     //   newGame();
@@ -51,7 +72,7 @@ for (vari =0; i< 12; i++){
 
 
 
-
+//     This was my old starting point before starting over. 
 // {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" script type="text/Javascript">
 // // <script src= "assets/Javascript.js" script type="text/Javascript">
 //     $(document).ready(function)()
@@ -78,7 +99,7 @@ for (vari =0; i< 12; i++){
 //     function resetGame(){
 //     var randomNumber = numberArray[Math.floor(random() * numberArray.length)];
 //     console.log(randomNumber);
-    
+
 // }
 // function resetCrystalValue() {
 //     var crystalNumber = crystalArray[Math.floor(Math.random()* crystalArray.length)];
